9f64904403ff88f864ab88ce5568e523c2dabd5a
kaushal.agarwal2016@vitstudent.ac.in <kaushalagarwal79@gmail.com>
2018-09-30 20:37:00 +0530

    Designing registration page

--- app/src/main/java/com/fullertonfinnovatica/Create.java
+++ app/src/main/java/com/fullertonfinnovatica/Create.java
@@ -2,12 +2,29 @@ package com.fullertonfinnovatica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Create extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideStatusBar();
         setContentView(R.layout.activity_create);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideStatusBar();
+    }
+
+    private void hideStatusBar()
+    {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN );
     }
 }
