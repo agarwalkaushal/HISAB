1cc01d60e252e12dbb9cf1bc62c7266c97ae5252
YK-Yash <yashkumar1998@gmail.com>
2018-12-10 15:42:47 +0530

    Made networking recycler view clickable

--- app/src/main/java/com/fullertonfinnovatica/Networking/NetworkingAPI.java
+++ app/src/main/java/com/fullertonfinnovatica/Networking/NetworkingAPI.java
@@ -8,9 +8,11 @@ import retrofit2.http.POST;
 
 public interface NetworkingAPI {
 
-    String BASE_URL = "http://192.168.43.8:3000/api/";
+    //String BASE_URL = "http://192.168.43.8:3000/api/";
+    String BASE_URL = "https://api.myjson.com/bins/";
 
-    @GET("points")
+//    @GET("points")
+    @GET("asdfq")
     Call<List<NetworkingModel>> getBirds();
 
 }
